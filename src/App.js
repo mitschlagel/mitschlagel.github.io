9f20ad98a816cca8e1866547916b05e1bb18aea6
Spencer Jones <sjonesdrums@gmail.com>
2024-01-02 00:10:38 -0600

    Add resume buttons

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import ContactForm from './ContactForm';
 
 import ReactGA from 'react-ga';
 import React from 'react';
+import FadeInModalButton from './FadeInModalButton';
 
 function App() {
 
-  ReactGA.initialize('G-P6NNXH3H9C');
+  // ReactGA.initialize('G-P6NNXH3H9C');
   return (
     <div className="App">
       <Main />
@@ -24,7 +25,7 @@ const Main = () => {
     <div className="main">
       <Header />
       <Headline />
-      <PrimaryContent />
+      <Resume />
       <SocialLinks />
     </div>
   )
@@ -35,7 +36,7 @@ const Header = () => {
     <header className="header">
         <div className="title">
           <h1>Spencer Jones </h1>
-          <h2>Musician, Engineer, & Educator</h2>
+          <h2>Musician, Software Engineer, & Educator</h2>
         </div>
         {/* <div className="contact-us">
           <ContactForm />
@@ -76,6 +77,19 @@ const PrimaryContent = () => {
   )
 }
 
+const Resume = () => { 
+  return (
+    <div className="resume">
+      <div className="resume-buttons-container">
+        <FadeInModalButton title="Performer" content={text1} />
+        <FadeInModalButton title="Teacher" content={"Teaching"} />
+        <FadeInModalButton title="Engineer" content={"Software"} />
+        <FadeInModalButton title="Education" content={"Education"} />
+      </div>
+    </div>
+  )
+}
+
 const SocialLinks = () => {
   return (
     <div className="social-links">
@@ -91,7 +105,7 @@ const SocialLinks = () => {
   
 
 export default App;
-const bigText = "Spencer is a multi-faceted creative professional who enjoys a dual career as an orchestral percussionist and software engineer."
+const bigText = "Spencer is a multi-faceted creative professional enjoying dual careers as an orchestral percussionist and software engineer."
 const text1 = "For over ten years he has served as a first-call musician with the Omaha and Kansas City Symphonies and has performed with the Grand Rapids Symphony, Indianapolis Symphony Orchestra, Hawaii Symphony Orchestra, and the New World Symphony of Miami Beach, FL. He has accompanied a diverse range of artists, including Yo-Yo Ma, Lyle Lovett, Joyce DiDonato, Guster, Branford Marsalis, Ben Folds, and Bernadette Peters."
 const text2 = "During the pandemic and shut down of the live music industry in 2020, Spencer returned to school, studying computer science at Creighton University and quickly securing a software engineering job in the banking sector."
 const text3 = "A passionate music educator and percussion advocate, Spencer is adjunct professor of percussion at the University of Nebraska-Omaha, teaching private lessons to undergraduate and graduate students. From 2016 - 2021, Spencer served on the Artist-Faculty at Omaha Conservatory of Music, where he grew the percussion studio from zero to twenty students in five years. His students routinely placed in All-State ensembles, earned college scholarships, and won acceptance into prestigious programs such as Interlochen and Boston University-Tanglewood Institute. Additionally, Spencer has served on the faculties of Iowa Western Community College and Dordt University, as well as on the percussion staff of Broken Arrow (OK) High School’s The Pride of Broken Arrow marching band."
