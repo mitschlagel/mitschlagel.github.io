eb5a7a3e90a81acd26fbf27049e015bf64350bf5
Spencer Jones <sjonesdrums@gmail.com>
2024-01-02 14:40:32 -0600

    Adjust responsiveness for resume button bar

--- src/App.js
+++ src/App.js
@@ -24,8 +24,9 @@ const Main = () => {
   return (
     <div className="main">
       <Header />
-      <Headline />
       <Resume />
+      <Headline />
+      <PrimaryContent />
       <SocialLinks />
     </div>
   )
@@ -36,7 +37,6 @@ const Header = () => {
     <header className="header">
         <div className="title">
           <h1>Spencer Jones </h1>
-          <h2>Musician, Software Engineer, & Educator</h2>
         </div>
         {/* <div className="contact-us">
           <ContactForm />
@@ -81,10 +81,9 @@ const Resume = () => {
   return (
     <div className="resume">
       <div className="resume-buttons-container">
-        <FadeInModalButton title="Performer" content={text1} />
-        <FadeInModalButton title="Teacher" content={"Teaching"} />
-        <FadeInModalButton title="Engineer" content={"Software"} />
-        <FadeInModalButton title="Education" content={"Education"} />
+        <FadeInModalButton title="Musician" content={text1} />
+        <FadeInModalButton title="Software Engineer" content={"Teaching"} />
+        <FadeInModalButton title="Teacher" content={"Software"} />
       </div>
     </div>
   )
@@ -108,5 +107,5 @@ export default App;
 const bigText = "Spencer is a multi-faceted creative professional enjoying dual careers as an orchestral percussionist and software engineer."
 const text1 = "For over ten years he has served as a first-call musician with the Omaha and Kansas City Symphonies and has performed with the Grand Rapids Symphony, Indianapolis Symphony Orchestra, Hawaii Symphony Orchestra, and the New World Symphony of Miami Beach, FL. He has accompanied a diverse range of artists, including Yo-Yo Ma, Lyle Lovett, Joyce DiDonato, Guster, Branford Marsalis, Ben Folds, and Bernadette Peters."
 const text2 = "During the pandemic and shut down of the live music industry in 2020, Spencer returned to school, studying computer science at Creighton University and quickly securing a software engineering job in the banking sector."
-const text3 = "A passionate music educator and percussion advocate, Spencer is adjunct professor of percussion at the University of Nebraska-Omaha, teaching private lessons to undergraduate and graduate students. From 2016 - 2021, Spencer served on the Artist-Faculty at Omaha Conservatory of Music, where he grew the percussion studio from zero to twenty students in five years. His students routinely placed in All-State ensembles, earned college scholarships, and won acceptance into prestigious programs such as Interlochen and Boston University-Tanglewood Institute. Additionally, Spencer has served on the faculties of Iowa Western Community College and Dordt University, as well as on the percussion staff of Broken Arrow (OK) High School’s The Pride of Broken Arrow marching band."
-const text4 = "Originally from Broken Arrow, Oklahoma, Spencer earned a BA in Music from Oklahoma State University and an MM in Percussion Performance with post-graduate studies in orchestral percussion from the University of Missouri–Kansas City Conservatory. Additionally, he studied computer science at Creighton University and web development at the AIM Institute in Omaha, NE. His primary teachers include Christopher McLaurin, Wayne Bovenschen, and Tom McGillen, and he is an alumnus of The Music Academy of the West, the National Orchestral Institute, the Texas Music Festival, and the Cloyd Duff Timpani Masterclass."
+const text3 = "A passionate music educator and percussion advocate, Spencer is adjunct professor of percussion at the University of Nebraska-Omaha and maintains a private studio of highly motivated high school students. From 2016-2021, Spencer served on the Artist-Faculty at Omaha Conservatory of Music, where he grew the percussion studio from zero to twenty students in five years. His students routinely placed in All-State ensembles, earned college scholarships, and won acceptance into prestigious programs such as Interlochen and Boston University-Tanglewood Institute. Additionally, Spencer has served on the faculties of Iowa Western Community College and Dordt University, as well as on the percussion staff of Broken Arrow (OK) High School’s The Pride of Broken Arrow marching band."
+const text4 = "Originally from Broken Arrow, Oklahoma, Spencer earned a BA in Music from Oklahoma State University and an MM in Percussion Performance with post-graduate studies in orchestral percussion from the University of Missouri–Kansas City Conservatory. From 2020-2022, he studied computer science at Creighton University in Omaha, NE. His primary teachers include Christopher McLaurin, Wayne Bovenschen, and Tom McGillen, and he is an alumnus of The Music Academy of the West, the National Orchestral Institute, the Texas Music Festival, and the Cloyd Duff Timpani Masterclass."
