9089b218132312f4e255209e1c79d26199b6b4d2
Spencer Jones <sjonesdrums@gmail.com>
2026-01-29 07:43:13 -0600

    Fix typo

--- src/assets/headerText.tsx
+++ src/assets/headerText.tsx
@@ -9,7 +9,6 @@ export const HeaderText: React.FC = () => {
         <p>
          For over 10 years I was a full-time orchestra musician and music teacher. I took over 40 professional auditions, which somehow makes me both an expert and a failure.  
         During the pandemic, as a respite from the loss of work, I rediscovered my high-school-era enthusaism for coding, watched too much Silicon Valley, and went back to school. Fast forward five whirlwind years and I'm a "senior" software engineer who still sometimes gets paid to play drums.
-        This is a place for me to share things I find interesting and explore my love of writing.
         </p>
         <br></br>
         <p> PS, If you feel stuck in the orchesta world and want a change, reach out. There are many of us, and we can help.
